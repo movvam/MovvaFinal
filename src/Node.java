2cfab78eb4a1f24ea737bc33bc5d2e9c602a8278
unknown <mmovva833@FHS-LAB127-LE33.fhs.win.fuhsd.org>
2016-05-10 08:54:14 -0700

    add some class variables

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+
+public class Node {
+
+	private int x, y;
+	private Location loc;
+	private int heuristic; 
+	private int moveCost;
+	private int fVal;
+	private Location parentNode;
+	
+	public Node(int x, int y, )
+}
