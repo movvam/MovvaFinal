fc4420e79cba386c1a98a5db8200949ea8087d8d
movvan <movvan>
2016-05-11 23:01:32 -0700

    added Location class

--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,13 @@
 
 public class Node {
 
-	private int x, y;
 	private Location loc;
 	private int heuristic; 
 	private int moveCost;
 	private int fVal;
 	private Location parentNode;
 	
-	public Node(int x, int y, )
+	public Node(Location loc){
+		this.loc = loc;
+	}
 }
